4afe059cafc501ffbacac2e7961b9cac9b893569
awesomedominic <113389613+awesomedominic@users.noreply.github.com>
2023-11-09 14:23:08 -0800

    Working on step 5

---
+++ Personal Project/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public GameObject powerup;
+
+    private float zEnemySpawn = 12.0f;
+    private float xSpawnBound = 16.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
