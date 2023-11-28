df7266bc25e8ac27ff6fb19d32bced5a6ce35b60
awesomedominic <113389613+awesomedominic@users.noreply.github.com>
2023-11-28 14:25:44 -0800

    Updating game files

--- Personal Project/Assets/Scripts/SpawnManager.cs
+++ Personal Project/Assets/Scripts/SpawnManager.cs
@@ -8,17 +8,28 @@ public class SpawnManager : MonoBehaviour
     public GameObject powerup;
 
     private float zEnemySpawn = 12.0f;
-    private float xSpawnBound = 16.0f;
+    private float xSpawnRange = 16.0f;
+    private float zPowerupRange = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnEnemy();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnEnemy()
+    {
+        float randomX = Random.Range(-xSpawnRange, xSpawnRange);
+        int randomIndex = Random.Range(0, enemies.Length);
+
+        Vector3 spawnPos = new Vector3(randomX, 0.75f, zEnemySpawn);
+
+        Instantiate(enemies[randomIndex], spawnPos, enemies[randomIndex].gameObject.transform.rotation);
     }
 }
